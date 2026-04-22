094ca6b7a9154f4d65cc1a01a680e6ebbbe6ff81
jonahirande <jonah.irande@bludive.net>
2026-04-22 10:09:08 +0000

    new code

--- db/init-db.js
+++ db/init-db.js
@@ -3,8 +3,8 @@ db = db.getSiblingDB('liveclinic');
 
 db.createUser({
   user: "clinic_admin",
-  pwd: "p@ssw0rd_db_user",
-  roles: [{ role: "readWrite", db: "liveclinic" }]
-});
+    pwd: "p@ssw0rd_db_user",
+      roles: [{ role: "readWrite", db: "liveclinic" }]
+      });
 
-db.createCollection('users');
+      db.createCollection('users');
